9dc3f8222f063cf55e8342b62af42e4155961807
srikanth65 <srikanthch0465@gmail.com>
2024-04-09 10:35:18 -0700

    k8-eksctl

--- workstation.tf
+++ workstation.tf
@@ -7,7 +7,7 @@ module "ec2_instance" {
   #key_name               = "user1"
   #monitoring             = true
   vpc_security_group_ids = [aws_security_group.allow_eksctl.id]
-  subnet_id              = "subnet-0ea509ad4cba242d7" #replace your default subnet id
+  subnet_id              = "subnet-09af1fc2b1fefd76d" #replace your default subnet id
   user_data = file("workstation.sh")
   tags = {
     Terraform   = "true"
